54e023a38e3529ed88498fca55fd575e742e0e11
facundomaksud@gmail.com <facundomaksud@gmail.com>
2021-10-21 11:21:21 -0300

    detail country modificado

--- client/src/pages/detailCountry/DetailCountry.jsx
+++ client/src/pages/detailCountry/DetailCountry.jsx
@@ -5,15 +5,14 @@ import { Link } from "react-router-dom";
 import { getCountryDetail } from "../../redux/actions";
 import styles from "./DetailCountry.module.css";
 
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
 
 export default function DetailCountry() {
   const country = useSelector((state) => state.countryDetail);
@@ -24,10 +23,10 @@ export default function DetailCountry() {
     dispatch(getCountryDetail(id.id));
   }, [dispatch, id.id]);
 
-   return (
+  return (
     <div align="center">
-        <Link to="/countries">
-        <div className={`btn btn-dark ${styles.button}`} >Back</div>
+      <Link to="/countries">
+        <div className={`btn btn-dark ${styles.button}`}>Back</div>
       </Link>
       <div className={styles.container}>
         <h3>DETAILS</h3>
@@ -53,60 +52,49 @@ export default function DetailCountry() {
         <p>
           <strong>TOURIST ACTIVITIES:</strong>
         </p>
-        { country.touristActivities?.length > 0 ?(
-        //   <table className={`${styles.tabla}  "table"`}>
-        //   <thead>
-        //     <td>NAME</td>
-        //     <td>DIFFICULTY</td>
-        //     <td>DURATION</td>
-        //     <td>SEASON</td>
-        //   </thead>
-        //   {
-            
-        //       country.touristActivities?.map((activitie, id) => (
-        //         <tr key={id}>
-        //           <td>{activitie.name}</td>
-        //           <td>{activitie.difficulty}</td>
-        //           <td>{activitie.duration}</td>
-        //           <td>{activitie.season}</td>
-        //         </tr>
-        //       )
-        //     )
-        //   }
-        // </table>)
-        <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 300 }} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="center">NAME</TableCell>
-            <TableCell align="center">DIFFICULTY</TableCell>
-            <TableCell align="center">DURATION</TableCell>
-            <TableCell align="center">SEASON</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {country.touristActivities?.map((activitie, id) => (
-            <TableRow
-              key={id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+        {country.touristActivities?.length > 0 ? (
+          <TableContainer component={Paper}>
+            <Table
+              sx={{ minWidth: 300 }}
+              size="small"
+              aria-label="a dense table"
             >
-              
-              <TableCell align="center">{activitie.name}</TableCell>
-              <TableCell align="center">{activitie.difficulty}</TableCell>
-              <TableCell align="center">{activitie.duration}</TableCell>
-              <TableCell align="center">{activitie.season}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-        )
-        : 
-        <div> <p> <strong>TOURIST ACTIVITIES NOT FOUND<Link to='/activity'> (CREATE A NEW ACTIVITY)</Link> </strong></p> </div>
-        }
-        
+              <TableHead>
+                <TableRow>
+                  <TableCell align="center">NAME</TableCell>
+                  <TableCell align="center">DIFFICULTY</TableCell>
+                  <TableCell align="center">DURATION</TableCell>
+                  <TableCell align="center">SEASON</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {country.touristActivities?.map((activitie, id) => (
+                  <TableRow
+                    key={id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell align="center">{activitie.name}</TableCell>
+                    <TableCell align="center">{activitie.difficulty}</TableCell>
+                    <TableCell align="center">{activitie.duration}</TableCell>
+                    <TableCell align="center">{activitie.season}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        ) : (
+          <div>
+            {" "}
+            <p>
+              {" "}
+              <strong>
+                TOURIST ACTIVITIES NOT FOUND
+                <Link to="/activity"> (CREATE A NEW ACTIVITY)</Link>{" "}
+              </strong>
+            </p>{" "}
+          </div>
+        )}
       </div>
-      
     </div>
   );
 }
